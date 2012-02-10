24ba89b7a116e9032ac3f9238395f2280f3e5429
Dmitry Vyal <akamaus@gmail.com>
2012-02-10 13:50:37 +0400

    cabalized source

--- src/Main.hs
+++ src/Main.hs
@@ -2,13 +2,14 @@
 import Crossword
 import Printer
 
+import Text.Printf
+
 import Data.List
 
 ws = -- ["abcd", "axy", "caz"]--, "ybze","baza"]
      ["мул","сом","енот","крот","лемур","койот","панда","тукан","мустанг","гиппопотам"]
      --["папа","мама","паша"]
 
-cr = []
 {-
 cr = [ Word (ws !! 0) (0,0) Hor,
        Word (ws !! 1) (0,0) Ver,
@@ -19,7 +20,10 @@ cr = [ Word (ws !! 0) (0,0) Hor,
 --out_crs = makeCrossword ws
 
 --main = mapM_ (print . length . runBuilder) $ take 8 wws
-main = mapM_ (print . length . buildCrosswords) $ take 8 wws
+main = mapM_ print_nth $ take 8 wws
+  where print_nth wl = do printf "All crosswords of %d words\n" (length wl)
+                          mapM printCrossword $ buildCrosswords wl
+
 -- mapM_ (const $ putChar '.') $ runBuilder ws
 
 --mine = mapM_ printCrossword $ snd $  runEnumerator enumerateCrosswords ["aaaaaaac","aaaab"]
